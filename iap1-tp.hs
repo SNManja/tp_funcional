151399c4408ba96a3b63bacdb3e6e69be3369074
cristalcodes <133605301+cristalcodes@users.noreply.github.com>
2023-05-18 10:27:38 -0300

    Update iap1-tp.hs

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -60,12 +60,15 @@ perteneceUsuario n t | n == fst t = True
 
 -- Ejercicios
 
+-- 1
 nombresDeUsuarios :: RedSocial -> [String]
 nombresDeUsuarios red | null (usuarios (red)) == True = [] 
                       | otherwise = [nombreDeUsuario (head (usuarios (red)))] ++ nombresDeUsuarios ((tail (usuarios (red))), relaciones (red), publicaciones (red))
 
 -- describir qué hace la función: la funcion utiliza una recursion, llamando al primer elemento de la lista de usuarios y concatenandolo al elemento siguiente.
 -- Esto se logra llamando a la misma funcion, pero con la lista de usuarios sin el primer elemento. La recursion termina cuando la lista de usuarios queda vacia.
+
+-- 2
 
 esElPrimero :: Usuario -> Relacion -> Bool
 esElPrimero n h | n == fst h = True
@@ -85,6 +88,8 @@ amigosDe red u | null (relaciones (red)) == True = []
 -- de la relacion, o al segundo. Ya que la funcion, va llamando recursivamente a todos los elementos de la lista relaciones que hay en la red, de forma
 -- que si el usuario pertenece a alguna de estas relaciones, agrega a una lista al otro usuario que estaba en esa relacion.
 
+-- 3
+
 longitudLista :: [t] -> Int
 longitudLista [] = 0
 longitudLista (x:xs) = 1 + longitudLista xs
@@ -94,6 +99,8 @@ cantidadDeAmigos red u = longitudLista (amigosDe red u)
 
 -- describir qué hace la función: en esta funcion, lo unico que hago, es generar la lista de amigos del usuario con la funcion anterior, y obtengo
 -- la longitud de la lista.
+
+-- 4
 
 subUsuarioConMasAmigos :: RedSocial -> Usuario -> Usuario
 subUsuarioConMasAmigos red a | null (usuarios (red)) == True = a
@@ -111,6 +118,8 @@ usuarioConMasAmigos red = subUsuarioConMasAmigos (tail(usuarios (red)),relacione
 -- aniguo valor. De esta forma, en a siempre va a quedar guardado el usuario con mas amigos, y cuando la lista de usuarios quede vacia, la funcion devuelve el 
 -- usuario que quedo almacenado en a.
 
+-- 5
+
 estaRobertoCarlos :: RedSocial -> Bool
 estaRobertoCarlos red | null (usuarios (red)) == True = False
                       | otherwise = if cantidadDeAmigos red (head (usuarios (red))) > 1000000 
@@ -120,6 +129,8 @@ estaRobertoCarlos red | null (usuarios (red)) == True = False
 -- describir qué hace la función: el ejercicio va chequeando reursivamente todos los usuarios en la red, analizando si alguno tiene mas de 1000000 de amigos.
 -- En el caso de que se cumpla, devuelve true. En el caso de que la lista de usuarios quede vacia antes de encontrar a alguien con mas de 1000000 de amigos,
 -- entonces significa que nadie tiene un numero mayor de amigos que esa cantidad, por lo tanto devuelve false.
+
+-- 6
 
 publicacionde :: Publicacion -> Usuario -> Bool
 publicacionde pub u | x1 == u = True
@@ -136,6 +147,8 @@ publicacionesDe red u | null (publicaciones (red)) == True = []
 -- dentro de la red, y a traves de la subfuncion "publicacionde", se fija si el usuario guardado en "u" es autor de la publicion. En el caso de que lo sea,
 -- esa publicacion es agregada a una lista, en el caso de que no lo sea, no. La recursion termina cuando el conjunto de publicaciones queda vacio, devolviendo 
 -- como resultado la lista formada con l¿todas las publicaciones del usuario.
+
+-- 7
 
 trd (_,_,a) = a
 
@@ -161,6 +174,8 @@ publicacionesQueLeGustanA red u = publicacionesLikeadas (publicaciones red) u
 -- publicaciones likeadas por el usuario. Gracias a esto, la funcion "publicacionesQueLeGustanA", unicamente tiene que llamar a la funcion anterior, poniendo
 -- como valores de entrada a la lista de publicaciones de la red y al usuario a evaluar.
 
+-- 8
+
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
 lesGustanLasMismasPublicaciones red u1 u2 | publicacionesQueLeGustanA red u1 == publicacionesQueLeGustanA red u2 = True
                                           | otherwise = False
@@ -170,6 +185,8 @@ lesGustanLasMismasPublicaciones red u1 u2 | publicacionesQueLeGustanA red u1 == 
 --   No hace falta analizar los casos en ue puedan tener los mismos elementos pero en distinto orden, ya que la funcion "publicacionesQueLeGustanA", va q recorrer
 -- las publicaciones de la reden el mismo orden tanto para u1 como para u2. De la misma forma, los elementos de ambas listas devueltas por la funcion mencionada,
 -- van a ser agregados en el mismo orden. 
+
+-- 9
 
 subTieneUnSeguidorFiel :: [Publicacion] -> [Usuario] -> Bool
 subTieneUnSeguidorFiel publicaciones usuarios | null usuarios == True = False
@@ -186,6 +203,8 @@ tieneUnSeguidorFiel red u = subTieneUnSeguidorFiel (publicacionesDe red u) (trd 
 -- la condicion de estar en la lista de usuarios que dieron like de todas las publicaciones. En el caso de que alguno si lo cumpla, devuelve True.
 --   Lo que hace la funcion principal es llamar a esta subfuncion, pero los valores de entrada son todas las publicaciones del usuario a analizar, y todos los
 -- usuarios que le dieron like a su primera publicacion(de esta forma evitamos analizar usuarios innecesarios).
+
+-- 10
 
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
 existeSecuenciaDeAmigos = undefined
